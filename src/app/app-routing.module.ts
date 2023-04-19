405944f605582b934ed3b457dc956bccab56222c
ghgupton <ghgupton@ncsu.edu>
2023-04-18 20:01:47 -0400

    Initial app with navigation and all base pages

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { FriendsComponent } from './friends/friends.component';
+
+const routes: Routes = [
+  { path: '', component: ProfileComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'friends', component: FriendsComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
